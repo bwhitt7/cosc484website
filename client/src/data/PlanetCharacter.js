640588668d232df2560bf07e5ddd0257b8150aef
Nidhi <144849351+ndesai97@users.noreply.github.com>
2023-12-05 01:59:42 -0500

    Planet Character path

---
+++ client/src/data/PlanetCharacter.js
@@ -0,0 +1,37 @@
+import sunC from "../images/Characters/Sun.png";
+import mercuryC from "../images/Characters/Mercury.png";
+import venusC from "../images/Characters/Venus.png";
+import earthC from "../images/Characters/Earth.png";
+import marsC from "../images/Characters/Mars.png";
+import jupiterC from "../images/Characters/Jupiter.png";
+import saturnC from "../images/Characters/Saturn.png";
+import uranusC from "../images/Characters/Uranus.png";
+import neptuneC from "../images/Characters/Neptune.png";
+import plutoC from "../images/Characters/Pluto.png";
+import moonC from "../images/Characters/Moon.png";
+import alienC from "../images/Characters/Alien.png";
+import astronautC from "../images/Characters/Astronaut.png";
+import meteorC from "../images/Characters/Meteor.png";
+import starC from "../images/Characters/Star.png";
+
+
+
+const PlanetCharacter = {
+    sunC,
+    mercuryC,
+    venusC,
+    earthC,
+    marsC,
+    jupiterC,
+    saturnC,
+    uranusC,
+    neptuneC,
+    plutoC,
+    moonC, 
+    alienC,
+    astronautC, 
+    meteorC,
+    starC 
+}
+
+export default PlanetCharacter;
